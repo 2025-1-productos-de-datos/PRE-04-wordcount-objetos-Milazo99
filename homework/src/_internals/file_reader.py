a7a7c47aa461ba6075493b84aef43c5fe3b97cc3
Camilo Montoya <camilom@solenium.co>
2025-05-17 08:44:56 -0500

    Pushing homework

---
+++ homework/src/_internals/file_reader.py
@@ -0,0 +1,16 @@
+import os
+
+class FileReader:
+
+    def __init__(self, input_folder):
+        self.input_folder = input_folder
+    
+    def read_all_lines(self):
+
+        lines = []
+
+        for filename in os.listdir(input_folder):
+            file_path = os.path.join(input_folder, filename)
+            with open(file_path, "r", encoding="utf-8") as f:
+                lines.extend(f.readlines())
+        return lines
