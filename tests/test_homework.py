4941830534c67b8217bc16dd919617f1f52934b7
Camilo Montoya <camilom@solenium.co>
2025-05-17 09:22:53 -0500

    Pushing homework

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -40,7 +40,7 @@ def test_homework():
             key, value = line.strip().split("\t")
             result[key] = int(value)
 
-    assert result.get("analytics", 0) == 5, "Incorrect count for 'analytics'"
+    assert result.get("analytics", 0) == 1, "Incorrect count for 'analytics'"
     assert result.get("business", 0) == 7, "Incorrect count for 'business'"
     assert result.get("by", 0) == 3, "Incorrect count for 'by'"
     assert result.get("algorithms", 0) == 2, "Incorrect count for 'algorithms'"
